033c46bb42ce11fc8950583ed76f005ffad4055c
岩瀬智史 <iwasesatoshi@iwasesatoshinoMacBook-Air.local>
2020-07-27 11:47:25 +0900

    application.rbのendの数が相違しエラー発生。修正しました。

--- config/application.rb
+++ config/application.rb
@@ -6,19 +6,20 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module ChatApp
-  class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+  module ChatApp
+    class Application < Rails::Application
+      # Initialize configuration defaults for originally generated Rails version.
+      config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-    config.generators do |g|
-      g.stylesheets false
-      g.javascripts false
-      g.helper false
-      g.test_framework false
+      # Settings in config/environments/* take precedence over those specified here.
+      # Application configuration can go into files in config/initializers
+      # -- all .rb files in that directory are automatically loaded after loading
+      # the framework and any gems in your application.
+      config.generators do |g|
+        g.stylesheets false
+        g.javascripts false
+        g.helper false
+        g.test_framework false
+    end
   end
 end
