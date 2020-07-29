235b53c5abe6fb29b45186f3064011ed78318159
岩瀬智史 <iwasesatoshi@iwasesatoshinoMacBook-Air.local>
2020-07-29 12:30:57 +0900

    チャットルームの削除機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # get 'messages/index'
   root "rooms#index"
   resources :users, omly: [:edit, :update]
-  resources :rooms, omly: [:new, :create] do
+  resources :rooms, omly: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
 
